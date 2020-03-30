81362b4614dccf383554f7245304be230a191c2a
Pietro Marino <pietro.marino_84@hotmail.it>
2020-03-30 22:21:22 +0200

    fix creazione app se già esistente

--- commands/create/createApp.js
+++ commands/create/createApp.js
@@ -1,19 +1,41 @@
 const Listr = require('listr');
 const { projectInstall } = require('pkg-install');
-const { mkdir, resolve } = require('../../utils/file');
+const { mkdir, resolve, exists } = require('../../utils/file');
 const copyskeletonApp = require('./copyskeletonApp');
 const copyTemplatesApp = require('./copyTemplatesApp');
 const { logErr, logSuccess } = require('../../utils/logger');
 const { init } = require('../../utils/git');
+const rimraf = require('rimraf');
+
+function checking(targetDir, name, force) {
+  return new Promise((resolve, reject) => {
+    try {
+      const dirAlreadyExists = exists(targetDir);
+      if (dirAlreadyExists) {
+        if (!force) throw new Error(name + ' application already exist. Add --force (-f) on your command to overwrite it.');
+        else {
+          rimraf(targetDir, (err) => {
+            if (err) throw new Error(err);
+            mkdir(targetDir);
+            resolve(true);
+          });
+        }
+      } else {
+        mkdir(targetDir);
+        resolve(true);
+      }
+    } catch (ex) {
+      reject(ex);
+    }
+  });
+}
 
 async function createApp(name, force) {
   const targetDir = resolve(process.cwd(), name);
 
-  // TODO: controllare qui se la cartella esiste già, o metterlo come primo task
-
   const tasks = new Listr([
     // create app directory
-    { title: `creating directory ${targetDir}`, task: () => mkdir(targetDir) },
+    { title: `checkinf if app already ${name} exists`, task: () => checking(targetDir, name, force) },
     // copy skeleton files
     { title: 'copying skeleton app', task: () => copyskeletonApp(targetDir) },
     // copy template files
@@ -27,7 +49,7 @@ async function createApp(name, force) {
 
   return tasks.run().then(() => {
     logSuccess(`
-      run cd ${name}
+      cd ${name}
       run npm start
       edit some files with your preferred editor and see the magic in action
     `);
